a47c728ed7f390664d420ed7839bf41dbaa54a64
CeezR <forthomme.cesar@gmail.com>
2023-04-15 22:30:42 +0200

    PayCalculator

---
+++ src/main/java/chapter4b/TwoNumberSum.java
@@ -0,0 +1,26 @@
+package chapter4b;
+
+import java.util.Scanner;
+
+public class TwoNumberSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean loop = true;
+        while (loop) {
+            // Ask for two numbers
+            System.out.print("Enter two number separated by a comma: ");
+            String[] numbers = scanner.next().split(",");
+            // Calculate SUM
+            double sum = Double.parseDouble(numbers[0]) + Double.parseDouble(numbers[1]);
+
+            // Display Result
+            System.out.printf("The sum of %s and %s is equal to %s", numbers[0], numbers[1], String.format("%.2f", sum));
+
+            // Ask for reset
+            System.out.println("\n Do you wish to continue. Type true for Yes, false for No");
+            loop = scanner.nextBoolean();
+        }
+
+    }
+}
